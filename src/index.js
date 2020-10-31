faa60b7e78748b0598b9948e7cc355689672e1e6
Ronan <liruonan@hotmail.com>
2020-10-31 20:02:59 +0800

    refactor: 真的跑起来了

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('Hello World from your main file!');
+import axios from 'axios'
+
+const GetUrl = 'https://www.baidu.com'
+
+axios(GetUrl)
+
+console.log('Hello World from your main file!')
